036fbc02310c4910e95d1b533d1018aef6aa929f
Cristina Dobre <crisfdobre@gmail.com>
2020-04-28 14:40:21 +0200

    New expercise monster app created

---
+++ docs/precache-manifest.5f454bf0157291ce2a6c39cc3e78bf9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6312298b940e1b1da46fab8b01a42574",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "b36ac23b87c04f453cf9",
+    "url": "/monster-app/static/css/main.3096d8e1.chunk.css"
+  },
+  {
+    "revision": "52ab36c179d2e860984b",
+    "url": "/monster-app/static/js/2.1662debb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.1662debb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b36ac23b87c04f453cf9",
+    "url": "/monster-app/static/js/main.1ffc64f3.chunk.js"
+  },
+  {
+    "revision": "0c571607ebdb9a69c537",
+    "url": "/monster-app/static/js/runtime-main.0e395675.js"
+  }
+]);
